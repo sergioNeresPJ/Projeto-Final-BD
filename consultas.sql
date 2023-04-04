866991295c070a5927c30368b07ccd4b32afe5a5
sergioNeresPJ <sergiomultitrack@gmail.com>
2023-04-04 13:19:43 -0300

    correçao consultas

--- consultas.sql
+++ consultas.sql
@@ -1,13 +1,14 @@
 -- Consulta 1
-SELECT * FROM item_cardapio WHERE preco > 35;
-
--- Consulta 2
 SELECT avg(salario) AS "media salarial" FROM funcionario;
 
--- Consulta 3
+-- Consulta 2
 SELECT p.id_pedido, i.nome_item FROM pedido_item p
 INNER JOIN ingrediente_prato i ON p.nome_item = i.nome_item
 WHERE nome_ingrediente = 'Ovo';
+
+-- Consulta 3
+SELECT avg(preco) AS "media de preco das entradas" FROM item_cardapio 
+INNER JOIN prato ON nome = nome_item WHERE tipo_prato = 'entrada';
 
 -- Consulta 4
 SELECT nome_ingrediente, max(quant) 
@@ -29,7 +30,7 @@ GROUP BY nome_ingrediente;
 SELECT nome FROM funcionario 
 INNER JOIN prepara_pedido ON cpf = cpf_funcionario
 INNER JOIN pedido ON id = id_pedido
-WHERE total = (SELECT max(total) FROM pedido);
+WHERE total = (SELECT max(total) FROM pedido)
 
 -- Consulta 6
 SELECT tipo_prato, count(*) quant FROM pedido_item NATURAL JOIN prato
